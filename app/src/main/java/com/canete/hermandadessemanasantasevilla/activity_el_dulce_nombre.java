4f5ff6cf8bc82a58c22e4705c65e6bb27fe9c2d3
alex <alex@canete.org>
2021-10-25 22:48:12 +0200

    cosas

---
+++ app/src/main/java/com/canete/hermandadessemanasantasevilla/activity_el_dulce_nombre.java
@@ -0,0 +1,14 @@
+package com.canete.hermandadessemanasantasevilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_el_dulce_nombre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_el_dulce_nombre);
+    }
+}
